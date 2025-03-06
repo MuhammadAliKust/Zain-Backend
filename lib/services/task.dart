9056e22edde073a4cf24922ccc85d2cdd76db7d9
Muhammad Ali Nizami <kustalics@gmail.com>
2025-03-06 10:46:59 +0500

    Update and Mark Task as Complete Done

--- lib/services/task.dart
+++ lib/services/task.dart
@@ -13,6 +13,13 @@ class TaskServices {
   ///Get Completed Tasks
   ///Get InComplete Tasks
   ///Update Task
+  Future updateTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .update({'title': model.title, 'description': model.description});
+  }
+
   ///Delete Task
   Future deleteTask(String taskID) async {
     return await FirebaseFirestore.instance
@@ -22,4 +29,10 @@ class TaskServices {
   }
 
   ///Mark Task as Complete
+  Future markTaskAsComplete(String taskID) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(taskID)
+        .update({'isCompleted': true});
+  }
 }
