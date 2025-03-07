0856096db52eff14225e6343a2cd8a38d8bc13d7
Muhammad Ali Nizami <kustalics@gmail.com>
2025-03-07 11:33:18 +0500

    Get All, Completed and InCompleted Task Done

--- lib/services/task.dart
+++ lib/services/task.dart
@@ -10,8 +10,37 @@ class TaskServices {
   }
 
   ///Get All Tasks
+  Stream<List<TaskModel>> getAllTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .snapshots()
+        .map((taskList) => taskList.docs
+            .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+            .toList());
+  }
+
   ///Get Completed Tasks
+  Stream<List<TaskModel>> getCompletedTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: true)
+        .snapshots()
+        .map((taskList) => taskList.docs
+            .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+            .toList());
+  }
+
   ///Get InComplete Tasks
+  Stream<List<TaskModel>> getInCompletedTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: false)
+        .snapshots()
+        .map((taskList) => taskList.docs
+            .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+            .toList());
+  }
+
   ///Update Task
   Future updateTask(TaskModel model) async {
     return await FirebaseFirestore.instance
